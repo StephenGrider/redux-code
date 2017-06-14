0fbc3b847a40c8008e299f7d4ee5c1916db319e3
stephengrider <ste.grider@gmail.com>
2017-06-14 13:43:03 -0700

    added import statements

---
+++ projects/youtube/src/index.js
@@ -0,0 +1,9 @@
+// Import any libraries or other files
+// that we need to use in this file
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a new React component
+
+// Render, or 'show', that component in the DOM
+// so that the user can see it
