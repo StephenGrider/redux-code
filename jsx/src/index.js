d11338ecafeb86e8e1243c8a939137fa59837984
stephen grider <ste.grider@gmail.com>
2018-10-30 16:47:44 -0600

    convert

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,7 +10,9 @@ const App = () => {
         Enter name:
       </label>
       <input id="name" type="text" />
-      <button style="background-color: blue; color: white;">Submit</button>
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        Submit
+      </button>
     </div>
   );
 };
