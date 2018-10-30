05b17f6d542bc4cb92b804f8363d73fd733de087
stephen grider <ste.grider@gmail.com>
2018-10-30 16:36:34 -0600

    convert

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,15 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label class="label" for="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style="background-color: blue; color: white;">Submit</button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
