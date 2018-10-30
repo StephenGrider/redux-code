ca372c490e8c9937c9036eb5ec6b528c106e9552
stephen grider <ste.grider@gmail.com>
2018-10-30 16:05:02 -0600

    display

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -3,5 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Create a react component
+const App = () => {
+  return <div>Hi there!</div>;
+};
 
 // Take the react component and show it on the screen
+ReactDOM.render(<App />, document.querySelector('#root'));
