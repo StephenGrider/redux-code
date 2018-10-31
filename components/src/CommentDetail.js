9c7d1e8ebb81dd5bbd919ded0972e9599b92306e
stephen grider <ste.grider@gmail.com>
2018-10-30 22:21:33 -0600

    extracted

---
+++ components/src/CommentDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CommentDetail = () => {
+  return (
+    <div className="comment">
+      <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+      </a>
+      <div className="content">
+        <a href="/" className="author">
+          Sam
+        </a>
+        <div className="metadata">
+          <span className="date">Today at 6:00PM</span>
+        </div>
+        <div className="text">Nice blog post!</div>
+      </div>
+    </div>
+  );
+};
