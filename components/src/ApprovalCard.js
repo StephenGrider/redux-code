066143fee19877f6bc0d7976894824187b9f1284
stephen grider <ste.grider@gmail.com>
2018-10-31 14:04:41 -0600

    children

--- components/src/ApprovalCard.js
+++ components/src/ApprovalCard.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const ApprovalCard = () => {
+const ApprovalCard = props => {
   return (
     <div className="ui card">
-      <div className="content">Are you sure?</div>
+      <div className="content">{props.children}</div>
       <div className="extra content">
         <div className="ui two buttons">
           <div className="ui basic green button">Approve</div>
--- components/src/index.js
+++ components/src/index.js
@@ -7,13 +7,15 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Sam"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog post"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
       <CommentDetail
         author="Alex"
         timeAgo="Today at 2:00AM"
