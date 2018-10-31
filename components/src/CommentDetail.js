0dc356ddbc9b3645cee62ac31fa6af4c7d35e196
stephen grider <ste.grider@gmail.com>
2018-10-31 11:56:01 -0600

    multiple

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -12,7 +12,7 @@ const CommentDetail = props => {
           {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
         <div className="text">Nice blog post!</div>
       </div>
--- components/src/index.js
+++ components/src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="Jane" />
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
+      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
+      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
     </div>
   );
 };
