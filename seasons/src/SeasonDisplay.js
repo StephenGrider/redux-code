33e1a233a62298b635e2237d87d9e921042afd9d
stephen grider <ste.grider@gmail.com>
2018-11-01 14:58:19 -0600

    getseason

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = props => {
-  console.log(props.lat);
+  const season = getSeason(props.lat, new Date().getMonth());
+
+  console.log(season);
 
   return <div>Season Display</div>;
 };
