3ba77e50b328f70c6515c8f7a54edfd942916b8f
stephen grider <ste.grider@gmail.com>
2018-10-31 21:12:22 -0600

    geolocation

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
+  );
+
   return <div>Hi there!</div>;
 };
 
