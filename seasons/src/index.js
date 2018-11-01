a54ee24b3298c2f7be00fd57bbdb09534d461f17
stephen grider <ste.grider@gmail.com>
2018-11-01 11:27:51 -0600

    conditional

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -7,20 +7,30 @@ class App extends React.Component {
 
     // THIS IS THE ONLY TIME we do direct assignment
     // to this.state
-    this.state = { lat: null };
+    this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
         // we called setstate!!!!
         this.setState({ lat: position.coords.latitude });
       },
-      err => console.log(err)
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
     );
   }
 
   // React says we have to define render!!
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
