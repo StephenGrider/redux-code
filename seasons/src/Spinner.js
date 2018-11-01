0afc5e46b76c744a57c6d7176937fe3a243133d9
stephen grider <ste.grider@gmail.com>
2018-11-01 15:40:51 -0600

    default props

--- seasons/src/Spinner.js
+++ seasons/src/Spinner.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-const Spinner = () => {
+const Spinner = props => {
   return (
     <div className="ui active dimmer">
-      <div className="ui big text loader">Loading...</div>
+      <div className="ui big text loader">{props.message}</div>
     </div>
   );
+};
+
+Spinner.defaultProps = {
+  message: 'Loading...'
 };
 
 export default Spinner;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -23,7 +23,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 
