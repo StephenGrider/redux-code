5deb207c27637b622f9ba9caf6a8d46a948f6321
stephen grider <ste.grider@gmail.com>
2018-11-02 12:24:11 -0600

    forms

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
