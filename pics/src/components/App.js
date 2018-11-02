71ac92aa3360b08f135480f0c5b3a8e71e15b1ef
stephen grider <ste.grider@gmail.com>
2018-11-02 17:20:05 -0600

    binding

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,7 +5,7 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
   state = { images: [] };
 
-  async onSearchSubmit(term) {
+  onSearchSubmit = async term => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
@@ -14,17 +14,13 @@ class App extends React.Component {
       }
     });
 
-    console.log(this);
     this.setState({ images: response.data.results });
-  }
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar
-          onSubmit={this.onSearchSubmit}
-          guesswhtIam="Im the propsobject"
-        />
+        <SearchBar onSubmit={this.onSearchSubmit} />
         Found: {this.state.images.length} images
       </div>
     );
