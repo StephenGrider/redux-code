cb3fa1f41cb44d87b952c611e37fd1d72e5894e1
stephen grider <ste.grider@gmail.com>
2018-11-02 16:48:29 -0600

    Axios

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  onSearchSubmit(term) {}
 
   render() {
     return (
