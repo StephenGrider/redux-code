fe5d3983726eb6e1dcfe6b0f915d63f2d35ba1d0
stephen grider <ste.grider@gmail.com>
2018-11-02 17:10:34 -0600

    async await

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: { query: term },
       headers: {
         Authorization:
           'Client-ID 34e39e5c2f447ce52009a515846ca2b6ccc35552bb63de59cf4a6d06728f3f7e'
       }
     });
+
+    console.log(response.data.results);
   }
 
   render() {
