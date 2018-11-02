ff1c568552bdb65c967e5fd56075d0322943b0c7
stephen grider <ste.grider@gmail.com>
2018-11-02 15:35:24 -0600

    solve context

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,11 +3,11 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onFormSubmit(event) {
+  onFormSubmit = event => {
     event.preventDefault();
 
     console.log(this.state.term);
-  }
+  };
 
   render() {
     return (
