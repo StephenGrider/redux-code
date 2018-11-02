da5a2aaf38f725ef223c3e50536917df0b88f31e
stephen grider <ste.grider@gmail.com>
2018-11-02 17:39:26 -0600

    render

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const ImageList = props => {
-  console.log(props.images);
+  const images = props.images.map(image => {
+    return <img src={image.urls.regular} />;
+  });
 
-  return <div>ImageList</div>;
+  return <div>{images}</div>;
 };
 
 export default ImageList;
