c222d3948d9671f2866dbe0c9fecc43ad1264c09
stephen grider <ste.grider@gmail.com>
2018-11-02 12:28:32 -0600

    styling

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,7 +3,7 @@ import SearchBar from './SearchBar';
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container" style={{ marginTop: '10px' }}>
       <SearchBar />
     </div>
   );
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -3,9 +3,12 @@ import React from 'react';
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>
-        <form>
-          <input type="text" />
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input type="text" />
+          </div>
         </form>
       </div>
     );
