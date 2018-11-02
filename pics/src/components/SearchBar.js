468a4dd03081b098107025dfdbd18e3d031e7e06
stephen grider <ste.grider@gmail.com>
2018-11-02 12:47:31 -0600

    evnet handler

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input type="text" onChange={this.onInputChange} />
           </div>
         </form>
       </div>
