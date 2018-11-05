1b6e4a612fcdccef395c4b9dc7f1c4451bca369d
stephen grider <ste.grider@gmail.com>
2018-11-05 14:18:37 -0700

    styling

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -5,7 +5,14 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
