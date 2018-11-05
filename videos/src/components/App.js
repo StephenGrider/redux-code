b3dc1145954ae36a1084bb09da001d3262fa51ce
stephen grider <ste.grider@gmail.com>
2018-11-05 11:54:44 -0700

    scaffold app

---
+++ videos/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ videos/src/components/SearchBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+  render() {
+    return <div>SearchBar</div>;
+  }
+}
+
+export default SearchBar;
