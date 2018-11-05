6c475f1379db64deb4d91d4bbdcb36b089b30017
stephen grider <ste.grider@gmail.com>
2018-11-05 12:41:03 -0700

    updating state

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -3,18 +3,23 @@ import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  onTermSubmit = term => {
-    youtube.get('/search', {
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const response = await youtube.get('/search', {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar onFormSubmit={this.onTermSubmit} />I have{' '}
+        {this.state.videos.length} videos.
       </div>
     );
   }
