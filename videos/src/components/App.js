1bb9f45cde36b0815c419aa71a8a21db9f8ba130
stephen grider <ste.grider@gmail.com>
2018-11-05 12:02:00 -0700

    event handler

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -4,7 +4,7 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui container">
         <SearchBar />
       </div>
     );
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: 'lkajflkajsdf' };
+
+  onInputChange = () => {};
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Video Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
