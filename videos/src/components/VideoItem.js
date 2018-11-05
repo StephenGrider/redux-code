5412fc396dee4ed0f5989b48e719d0616ee4ebf7
stephen grider <ste.grider@gmail.com>
2018-11-05 13:32:19 -0700

    list

---
+++ videos/src/components/VideoItem.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoItem = props => {
+  return <div>Video Item</div>;
+};
+
+export default VideoItem;
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import VideoItem from './VideoItem';
 
-const VideoList = props => {
-  return <div>{props.videos.length}</div>;
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map(video => {
+    return <VideoItem />;
+  });
+
+  return <div>{renderedList}</div>;
 };
 
 export default VideoList;
