08b16c7a2161ea49d9f69ef0ef4d61c801eb7fa5
stephen grider <ste.grider@gmail.com>
2018-11-05 14:29:36 -0700

    display

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -5,8 +5,13 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
       <div className="ui segment">
         <h4 className="ui header">{video.snippet.title}</h4>
         <p>{video.snippet.description}</p>
