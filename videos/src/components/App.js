bf298fea9fbf7227d53eb8276f8ad1ca3f6bfa86
stephen grider <ste.grider@gmail.com>
2018-11-05 13:27:18 -0700

    passing

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 class App extends React.Component {
   state = { videos: [] };
@@ -18,8 +19,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />I have{' '}
-        {this.state.videos.length} videos.
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ videos/src/components/VideoList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const VideoList = props => {
+  return <div>{props.videos.length}</div>;
+};
+
+export default VideoList;
