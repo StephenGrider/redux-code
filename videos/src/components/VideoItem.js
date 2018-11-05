dc7e053eccb7daeb36806e4e2f9b7e6e0e76ba35
stephen grider <ste.grider@gmail.com>
2018-11-05 13:48:54 -0700

    styling

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,7 +1,15 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = props => {
-  return <div>Video Item</div>;
+const VideoItem = ({ video }) => {
+  return (
+    <div className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <div className="content">
+        <div className="header">{video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -3,10 +3,10 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos }) => {
   const renderedList = videos.map(video => {
-    return <VideoItem />;
+    return <VideoItem video={video} />;
   });
 
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
