c62b33a9fe24ca8d6656ea64c244e94e6c4ca0e5
stephen grider <ste.grider@gmail.com>
2018-11-06 20:52:58 -0700

    config

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -3,8 +3,13 @@ import { connect } from 'react-redux';
 
 class SongList extends Component {
   render() {
+    console.log(this.props);
     return <div>SongList</div>;
   }
 }
 
-export default connect()(SongList);
+const mapStateToProps = state => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
