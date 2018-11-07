096d50792cf88b877b3dd72cbe13626b55e27336
stephen grider <ste.grider@gmail.com>
2018-11-07 11:12:29 -0700

    list

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -3,8 +3,12 @@ import SongList from './SongList';
 
 const App = () => {
   return (
-    <div>
-      <SongList />
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+      </div>
     </div>
   );
 };
