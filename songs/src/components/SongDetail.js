cb35f47ad8d69919be0079641a1bc66d8ded3075
stephen grider <ste.grider@gmail.com>
2018-11-07 12:05:28 -0700

    conditional

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = props => {
-  console.log(props);
-  return <div>Song Detail</div>;
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>;
+  }
+
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {song.title}
+        <br />
+        Duration: {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = state => {
