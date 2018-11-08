2e254ff352f07b8c99e0b6e709011e0b6184ce33
stephen grider <ste.grider@gmail.com>
2018-11-08 16:13:31 -0700

    restore ac

--- pagespeed/src/actions/index.js
+++ pagespeed/src/actions/index.js
@@ -1,7 +1,12 @@
 import google from '../apis/google';
 
-export const fetchReport = url => {
-  google.get('/runPagespeed', {
+export const fetchReport = async url => {
+  const response = await google.get('/runPagespeed', {
     params: { url }
   });
+
+  return {
+    type: 'FETCH_REPORT',
+    payload: response
+  };
 };
