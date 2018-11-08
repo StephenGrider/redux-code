6361192f5070e11ef812c0a5666dcd1d99a2826f
stephen grider <ste.grider@gmail.com>
2018-11-08 14:40:14 -0700

    reducers

--- pagespeed/src/index.js
+++ pagespeed/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={createStore(() => 5)}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
