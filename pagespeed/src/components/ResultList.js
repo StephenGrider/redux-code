8f5a95e1ad519f6e00b81ac796faa34ffba0c0e2
stephen grider <ste.grider@gmail.com>
2018-11-09 15:09:03 -0700

    summaries

--- pagespeed/src/components/ResultList.js
+++ pagespeed/src/components/ResultList.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class ResultList extends React.Component {
+  renderSummary(result) {
+    console.log(result);
+  }
+
   render() {
     const results = Object.values(
       this.props.report.formattedResults.ruleResults
     );
 
     return results.map(result => {
-      return <div>{result.localizedRuleName}</div>;
+      return (
+        <div className="active step" key={result.localizedRuleName}>
+          <div className="content">
+            <h3>{result.localizedRuleName}</h3>
+            <p>{this.renderSummary(result)}</p>
+          </div>
+        </div>
+      );
     });
   }
 }
