103e198ca1964fc3c97f8f8c42d1bd6d162a37c5
stephen grider <ste.grider@gmail.com>
2018-11-12 14:23:37 -0700

    single purpose

--- pagespeed/src/reducers/reportsReducer.js
+++ pagespeed/src/reducers/reportsReducer.js
@@ -1,15 +1,38 @@
 import { combineReducers } from 'redux';
 
-const dataReducer = (state, action) => {
-  return [];
+const dataReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_REPORT_SUCCESS':
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
 
-const loadingReducer = (state, action) => {
-  return false;
+const loadingReducer = (state = false, action) => {
+  switch (action.type) {
+    case 'FETCH_REPORT':
+      return true;
+    case 'FETCH_REPORT_SUCCESS':
+      return false;
+    case 'FETCH_REPORT_ERROR':
+      return false;
+    default:
+      return state;
+  }
 };
 
-const errorReducer = (state, action) => {
-  return null;
+const errorReducer = (state = null, action) => {
+  switch (action.type) {
+    case 'FETCH_REPORT':
+      return null;
+    case 'FETCH_REPORT_SUCCESS':
+      return null;
+    case 'FETCH_REPORT_ERROR':
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 export default combineReducers({
