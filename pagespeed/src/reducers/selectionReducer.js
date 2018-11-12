ca52d2373f17bdeea1bfab7784e942365219d72d
stephen grider <ste.grider@gmail.com>
2018-11-12 13:58:57 -0700

    quick update

--- pagespeed/src/reducers/selectionReducer.js
+++ pagespeed/src/reducers/selectionReducer.js
@@ -2,7 +2,7 @@ const selectionReducer = (state = null, action) => {
   switch (action.type) {
     case 'SELECT_REPORT':
       return action.payload;
-    case 'FETCH_REPORT':
+    case 'FETCH_REPORT_SUCCESS':
       return action.payload.id;
     default:
       return state;
