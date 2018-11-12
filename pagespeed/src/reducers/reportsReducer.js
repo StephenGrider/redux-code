bcd0da678d411b782db37530dabffc5999fae46c
stephen grider <ste.grider@gmail.com>
2018-11-12 14:05:14 -0700

    multiple

--- pagespeed/src/reducers/reportsReducer.js
+++ pagespeed/src/reducers/reportsReducer.js
@@ -1,7 +1,21 @@
-const reportsReducer = (state = [], action) => {
+// Approach #1 - seen in blog posts and tutorials!!!
+
+const INITIAL_STATE = {
+  data: [],
+  loading: false,
+  error: null
+};
+
+const reportsReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'FETCH_REPORT':
-      return [...state, action.payload];
+      return { ...state, loading: true };
+    case 'FETCH_REPORT_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        data: [...state.data, action.payload]
+      };
     default:
       return state;
   }
