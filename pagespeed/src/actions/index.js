e492890a9f2572afe70f2edb18e465c16d100445
stephen grider <ste.grider@gmail.com>
2018-11-12 13:49:32 -0700

    dispatching

--- pagespeed/src/actions/index.js
+++ pagespeed/src/actions/index.js
@@ -2,11 +2,17 @@ import google from '../apis/google';
 
 export const fetchReport = url => {
   return async function(dispatch, getState) {
-    const response = await google.get('/runPagespeed', {
-      params: { url }
-    });
+    dispatch({ type: 'FETCH_REPORT' });
 
-    dispatch({ type: 'FETCH_REPORT', payload: response.data });
+    try {
+      const response = await google.get('/runPagespeed', {
+        params: { url }
+      });
+
+      dispatch({ type: 'FETCH_REPORT_SUCCESS', payload: response.data });
+    } catch (err) {
+      dispatch({ type: 'FETCH_REPORT_ERROR', payload: 'Failed to fetch URL.' });
+    }
   };
 };
 
