c364b7a57cadfa98603ed920006cf9e4666661d4
stephen grider <ste.grider@gmail.com>
2018-11-13 14:10:26 -0700

    wiring

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts } from '../actions';
 
 class PostList extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
   render() {
     return <div>Post List</div>;
   }
 }
 
-export default PostList;
+export default connect(
+  null,
+  { fetchPosts }
+)(PostList);
