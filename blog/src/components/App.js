a32a466cca2278a68d30b152bc42f8c7b1412fb6
stephen grider <ste.grider@gmail.com>
2018-11-13 13:50:07 -0700

    setup

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PostList from './PostList';
 
 const App = () => {
-  return <div className="ui container">App</div>;
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
 };
 
 export default App;
---
+++ blog/src/components/PostList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class PostList extends React.Component {
+  render() {
+    return <div>Post List</div>;
+  }
+}
+
+export default PostList;
