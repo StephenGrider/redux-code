5d9c7e5a85822bbb8e5884379c8cbee4c8dfa95c
stephen grider <ste.grider@gmail.com>
2018-11-13 12:33:12 -0700

    setup

---
+++ blog/src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div className="ui container">App</div>;
+};
+
+export default App;
---
+++ blog/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
+
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
