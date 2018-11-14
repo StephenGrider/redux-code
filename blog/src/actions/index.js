2316ae99c0fedad325ef40ca8a83a4235f950f1d
stephen grider <ste.grider@gmail.com>
2018-11-14 11:00:54 -0700

    shorten ac

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
-  // Bad approach!!!!
+export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
-  return {
-    type: 'FETCH_POSTS',
-    payload: response
-  };
+  dispatch({ type: 'FETCH_POSTS', payload: response });
 };
