cc9779603b5c411435460d18dd810602980f6ee6
stephen grider <ste.grider@gmail.com>
2018-11-14 16:51:02 -0700

    display

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
   componentDidMount() {
@@ -17,6 +18,7 @@ class PostList extends React.Component {
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
         </div>
       );
