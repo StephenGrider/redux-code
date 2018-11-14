5ca4dc91ca9b96a20639c10b1ff9e72aa24ae6be
stephen grider <ste.grider@gmail.com>
2018-11-14 15:40:49 -0700

    dispatch

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -8,11 +8,16 @@ class PostList extends React.Component {
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchPosts }
 )(PostList);
