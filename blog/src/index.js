2f2053f9cc1a28ee8f0d824f556af62f0e43b033
stephen grider <ste.grider@gmail.com>
2018-11-14 11:00:54 -0700

    shorten ac

--- blog/src/index.js
+++ blog/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const store = createStore(reducers, applyMiddleware(thunk));
+
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector('#root')
