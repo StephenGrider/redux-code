bdf253a1cdd152ea2606c2d6c53e4f8ea5b67d3c
stephen grider <ste.grider@gmail.com>
2018-11-15 14:44:06 -0700

    unique

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-  componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     const { user } = this.props;
 
@@ -22,7 +17,4 @@ const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
-export default connect(
-  mapStateToProps,
-  { fetchUser }
-)(UserHeader);
+export default connect(mapStateToProps)(UserHeader);
