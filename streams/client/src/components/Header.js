c094df189dcfc0a0abf767db3d88a68c6fdb64eb
stephen grider <ste.grider@gmail.com>
2018-11-20 13:55:39 -0700

    wire up gapi

--- streams/client/src/components/Header.js
+++ streams/client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GoogleAuth from './GoogleAuth';
 
 const Header = () => {
   return (
@@ -11,6 +12,7 @@ const Header = () => {
         <Link to="/" className="item">
           All Streams
         </Link>
+        <GoogleAuth />
       </div>
     </div>
   );
