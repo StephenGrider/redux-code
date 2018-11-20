a8fbfead091af642970d630d0ab8b80acf3b4f74
stephen grider <ste.grider@gmail.com>
2018-11-20 16:37:17 -0700

    userid

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -22,7 +22,7 @@ class GoogleAuth extends React.Component {
 
   onAuthChange = isSignedIn => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
