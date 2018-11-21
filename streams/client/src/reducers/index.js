7fe990a2b38bad695deca8d414a8f0ad68390ca2
stephen grider <ste.grider@gmail.com>
2018-11-21 11:53:09 -0700

    conecting

--- streams/client/src/reducers/index.js
+++ streams/client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });
