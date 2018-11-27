7644224df3a1ffb3372733273da4c74b1612f815
stephen grider <ste.grider@gmail.com>
2018-11-27 12:01:39 -0700

    omit

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import {
   FETCH_STREAM,
   FETCH_STREAMS,
@@ -14,6 +15,8 @@ export default (state = {}, action) => {
       return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
       return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
     default:
       return state;
   }
