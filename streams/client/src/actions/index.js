8ae1dc3ab6516698d0bc6f601134f3ba59830806
stephen grider <ste.grider@gmail.com>
2018-11-27 15:20:18 -0700

    implement

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -27,8 +28,7 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
-  // Do some programmatic navigation to
-  // get the user back to the root route
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
