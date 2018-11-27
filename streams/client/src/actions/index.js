58cf2f2c6554411d8296400584c305625b0baeba
stephen grider <ste.grider@gmail.com>
2018-11-27 14:24:49 -0700

    associating

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -22,8 +22,9 @@ export const signOut = () => {
   };
 };
 
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
