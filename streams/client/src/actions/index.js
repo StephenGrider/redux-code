c77ae3b29a5a51a04aef99aeadd472c2a4d3710b
stephen grider <ste.grider@gmail.com>
2018-11-27 15:01:56 -0700

    when

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -27,6 +27,8 @@ export const createStream = formValues => async (dispatch, getState) => {
   const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  // Do some programmatic navigation to
+  // get the user back to the root route
 };
 
 export const fetchStreams = () => async dispatch => {
