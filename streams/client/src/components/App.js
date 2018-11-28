360e34315a7e772d454eedfd584586fc4bde4cb4
stephen grider <ste.grider@gmail.com>
2018-11-28 11:55:04 -0700

    routing

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -16,7 +16,7 @@ const App = () => {
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
