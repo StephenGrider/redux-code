0e4bb4f721fe2c00ea70e9e10ef107c322b442c1
stephen grider <ste.grider@gmail.com>
2018-11-28 15:22:29 -0700

    change to patch req

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -44,9 +44,10 @@ export const fetchStream = id => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-  const response = await streams.put(`/streams/${id}`, formValues);
+  const response = await streams.patch(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const deleteStream = id => async dispatch => {
