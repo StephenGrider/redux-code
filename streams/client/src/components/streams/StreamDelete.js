6f72b510054428a3fa7f3adfe06840c6b9712f89
stephen grider <ste.grider@gmail.com>
2018-11-29 11:42:40 -0700

    fragments

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -3,10 +3,10 @@ import Modal from '../Modal';
 
 const StreamDelete = () => {
   const actions = (
-    <div>
+    <React.Fragment>
       <button className="ui button negative">Delete</button>
       <button className="ui button">Cancel</button>
-    </div>
+    </React.Fragment>
   );
 
   return (
