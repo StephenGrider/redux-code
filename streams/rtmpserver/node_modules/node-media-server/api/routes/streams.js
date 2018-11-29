13c403acdda498662f53748d4f06809b6842502a
stephen grider <ste.grider@gmail.com>
2018-11-29 16:31:12 -0700

    oops

--- streams/rtmpserver/node_modules/node-media-server/api/routes/streams.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const streamController = require('../controllers/streams');
-
-module.exports = (context) => {
-  let router = express.Router();
-  router.get('/', streamController.getStreams.bind(context));
-  router.get('/:app/:stream', streamController.getStream.bind(context));
-  return router;
-};
