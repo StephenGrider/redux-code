0dda4543f4029c675cd1755355b6f60012a5c32d
stephen grider <ste.grider@gmail.com>
2018-11-29 11:55:23 -0700

    params

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </Router>
