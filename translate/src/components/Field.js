7ca210d1b572894d8330c2792d5b6aad3ac87883
stephen grider <ste.grider@gmail.com>
2018-12-03 17:17:11 -0700

    consuming

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Field extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    const text = this.context === 'english' ? 'Name' : 'Naam';
+
     return (
       <div className="ui field">
-        <label>Name</label>
+        <label>{text}</label>
         <input />
       </div>
     );
