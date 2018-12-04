fe7916ed940533191aa44be3f80e1d50d7e8c689
stephen grider <ste.grider@gmail.com>
2018-12-04 12:28:43 -0700

    business

--- translate/src/contexts/LanguageContext.js
+++ translate/src/contexts/LanguageContext.js
@@ -1,3 +1,20 @@
 import React from 'react';
 
-export default React.createContext('english');
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <Context.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </Context.Provider>
+    );
+  }
+}
+export default Context;
