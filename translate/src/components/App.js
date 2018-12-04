456cd1e5d377977cb11dcd24563104ac90922603
stephen grider <ste.grider@gmail.com>
2018-12-03 17:36:41 -0700

    provider

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
 
 class App extends React.Component {
   state = { language: 'english' };
@@ -22,7 +23,9 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange('dutch')}
           />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
