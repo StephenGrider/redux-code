cfe67920775a0909c4a84dd5e9bfcd8e5ef942a5
stephen grider <ste.grider@gmail.com>
2018-12-03 17:08:43 -0700

    creating

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);
     return <button className="ui button primary">Submit</button>;
   }
 }
