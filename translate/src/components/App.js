1b0d1ad7e79bd5d628b4f6eaa5be5f676b5350bb
stephen grider <ste.grider@gmail.com>
2018-12-04 12:47:09 -0700

    rendering

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
-  state = { language: 'english' };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
+        <LanguageStore>
+          <LanguageSelector />
 
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={this.state.language}>
+          <ColorContext.Provider value="red">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
--- translate/src/contexts/LanguageContext.js
+++ translate/src/contexts/LanguageContext.js
@@ -11,7 +11,9 @@ export class LanguageStore extends React.Component {
 
   render() {
     return (
-      <Context.Provider value={{ ...this.state, onLanguageChange }}>
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}
+      >
         {this.props.children}
       </Context.Provider>
     );
