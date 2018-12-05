460e7d013a0d39fb77b0be086b5cfa942621d004
stephen grider <ste.grider@gmail.com>
2018-12-05 15:39:48 -0700

    gotcha

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -4,17 +4,15 @@ import axios from 'axios';
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async resource => {
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${resource}`
-    );
-
-    setResources(response.data);
-  };
-
   useEffect(
     () => {
-      fetchResource(resource);
+      (async resource => {
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/${resource}`
+        );
+
+        setResources(response.data);
+      })(resource);
     },
     [resource]
   );
