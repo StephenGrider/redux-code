04c55484f4dfee2070d7d098f7a6d9d2d07864ce
stephen grider <ste.grider@gmail.com>
2018-12-05 15:41:36 -0700

    resuse

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -17,7 +17,13 @@ const ResourceList = ({ resource }) => {
     [resource]
   );
 
-  return <div>{resources.length}</div>;
+  return (
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default ResourceList;
