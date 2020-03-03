24cff8bca5d816ee3b8e9d86cec3ec27954374e8
stephen grider <ste.grider@gmail.com>
2020-03-03 15:35:54 -0700

    select lang

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = { language: 'english' };
+
+  onLanguageChange = language => {
+    this.setState({ language });
+  };
+
   render() {
-    return <div className="ui container">App</div>;
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
+        </div>
+        {this.state.language}
+      </div>
+    );
   }
 }
 
