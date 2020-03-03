84a4a470b57bbdfb33f0c2f76a7e7359d4c5140c
stephen grider <ste.grider@gmail.com>
2020-03-03 15:41:42 -0700

    more setup

---
+++ translate/src/components/Field.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Field extends React.Component {
+  render() {
+    return (
+      <div className="ui field">
+        <label>Name</label>
+        <input />
+      </div>
+    );
+  }
+}
+
+export default Field;
