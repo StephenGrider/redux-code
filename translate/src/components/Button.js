0c05615ef46546deb6dbdaaf7218598c39f568e2
stephen grider <ste.grider@gmail.com>
2020-03-04 14:42:07 -0700

    connecting

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,15 +3,15 @@ import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === 'english' ? 'Submit' : 'Voorleggen';
+  renderSubmit(language) {
+    return language === 'english' ? 'Submit' : 'Voorleggen';
   }
 
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContext.Consumer>
-          {value => this.renderSubmit(value)}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext.Consumer>
       </button>
     );
--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
   static contextType = LanguageContext;
 
   render() {
-    const text = this.context === 'english' ? 'Name' : 'Naam';
+    const text = this.context.language === 'english' ? 'Name' : 'Naam';
 
     return (
       <div className="ui field">
