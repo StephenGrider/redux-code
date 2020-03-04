7e6b9106a59c7959b56b8b606e1388b881fac787
stephen grider <ste.grider@gmail.com>
2020-03-04 11:04:07 -0700

    helper method

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -2,11 +2,15 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+
   render() {
     return (
       <button className="ui button primary">
         <LanguageContext.Consumer>
-          {value => (value === 'english' ? 'Submit' : 'Voorleggen')}
+          {value => this.renderSubmit(value)}
         </LanguageContext.Consumer>
       </button>
     );
