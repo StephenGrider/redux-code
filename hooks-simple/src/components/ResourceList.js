050111674091c33358c4e4ed016fd184621e119f
stephen grider <ste.grider@gmail.com>
2020-03-05 14:36:00 -0700

    didupdate

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+  state = { resources: [] };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+
+    this.setState({ resources: response.data });
+  }
+
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource !== this.props.resource) {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+
+      this.setState({ resources: response.data });
+    }
+  }
+
   render() {
-    return <div>{this.props.resource}</div>;
+    return <div>{this.state.resources.length}</div>;
   }
 }
 
