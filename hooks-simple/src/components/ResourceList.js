3fcbfb95736596b277481495d7ad67d7bc298a9e
stephen grider <ste.grider@gmail.com>
2020-03-05 15:29:40 -0700

    updates

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -12,9 +12,12 @@ const ResourceList = ({ resource }) => {
     setResources(response.data);
   };
 
-  useEffect(() => {
-    fetchResource(resource);
-  }, []);
+  useEffect(
+    () => {
+      fetchResource(resource);
+    },
+    [resource]
+  );
 
   return <div>{resources.length}</div>;
 };
