03859fee2568fa5125c8b8795fcb43dd058fa992
stephen grider <ste.grider@gmail.com>
2020-03-05 15:04:09 -0700

    lifecycles

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const ResourceList = () => {
+const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([]);
 
-  const fetchResource = async () => {
+  const fetchResource = async resource => {
     const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/${this.props.resource}`
+      `https://jsonplaceholder.typicode.com/${resource}`
     );
 
-    this.setState({ resources: response.data });
+    setResources(response.data);
   };
+
+  useEffect(() => {
+    fetchResource(resource);
+  }, []);
 
   return <div>{resources.length}</div>;
 };
