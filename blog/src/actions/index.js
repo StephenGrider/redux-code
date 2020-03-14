bfb901eb4bd67e3657bff2347b0b65a0d89cd844
stephen grider <ste.grider@gmail.com>
2020-03-14 16:45:00 -0700

    singular

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -5,3 +5,9 @@ export const fetchPosts = () => async dispatch => {
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
