40f687bc1b9edc53f79abbc2fd9e5d1fd73c3f4e
stephen grider <ste.grider@gmail.com>
2020-03-14 15:40:49 -0700

    dispatch

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,5 +3,5 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ type: 'FETCH_POSTS', payload: response });
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
