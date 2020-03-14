76c8b40858d2ceac450ef9e35d6543ddeea73a32
stephen grider <ste.grider@gmail.com>
2020-03-14 15:29:20 -0700

    switch

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,3 +1,8 @@
-export default () => {
-  return undefined;
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
+  }
 };
