9920bf35fe9dfe1809d95cfa0a838ee01805648e
stephen grider <ste.grider@gmail.com>
2020-03-15 12:17:45 -0700

    one time memoize

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -7,16 +7,9 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-// export const fetchUser = id => async dispatch => {
-//   const response = await jsonPlaceholder.get(`/users/${id}`);
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
 
-//   dispatch({ type: 'FETCH_USER', payload: response.data });
-// };
-
-export const fetchUser = function(id) {
-  return _.memoize(async function(dispatch) {
-    const response = await jsonPlaceholder.get(`/users/${id}`);
-
-    dispatch({ type: 'FETCH_USER', payload: response.data });
-  });
-};
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+});
