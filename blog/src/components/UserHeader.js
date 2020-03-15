a59fd21896b3a996f1d3b99bed51c8ce074def3d
stephen grider <ste.grider@gmail.com>
2020-03-14 17:08:17 -0700

    extracting logic

--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -8,7 +8,7 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    const user = this.props.users.find(user => user.id === this.props.userId);
+    const { user } = this.props;
 
     if (!user) {
       return null;
@@ -18,8 +18,8 @@ class UserHeader extends React.Component {
   }
 }
 
-const mapStateToProps = state => {
-  return { users: state.users };
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
 export default connect(
