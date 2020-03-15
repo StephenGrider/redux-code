13ac38e276a2de923616474f1064c1a26f9fb5b6
stephen grider <ste.grider@gmail.com>
2020-03-15 14:44:06 -0700

    unique

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,11 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  console.log('About to fetch posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
-  console.log('fetched posts!');
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
 };
 
 export const fetchPosts = () => async dispatch => {
