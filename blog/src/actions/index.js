f64590c90855b0bcf8836d6c8a497ccfc3ed1d68
stephen grider <ste.grider@gmail.com>
2020-03-15 12:12:31 -0700

    issues

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
@@ -6,8 +7,16 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = id => async dispatch => {
-  const response = await jsonPlaceholder.get(`/users/${id}`);
+// export const fetchUser = id => async dispatch => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
 
-  dispatch({ type: 'FETCH_USER', payload: response.data });
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// };
+
+export const fetchUser = function(id) {
+  return _.memoize(async function(dispatch) {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+  });
 };
