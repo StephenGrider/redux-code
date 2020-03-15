7229d4c3613a2d069098e96b6279ebbf82eb5733
stephen grider <ste.grider@gmail.com>
2020-03-15 14:48:42 -0700

    chain

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,8 +4,11 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async dispatch => {
