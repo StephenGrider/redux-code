6535500d59adf11baabc0fac29ef203e2c7d4535
stephen grider <ste.grider@gmail.com>
2020-03-20 16:08:36 -0700

    connecting

---
+++ streams/client/src/actions/index.js
@@ -0,0 +1,11 @@
+export const signIn = () => {
+  return {
+    type: 'SIGN_IN'
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: 'SIGN_OUT'
+  };
+};
--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
 class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
@@ -19,8 +21,12 @@ class GoogleAuth extends React.Component {
     });
   }
 
-  onAuthChange = () => {
-    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut();
+    }
   };
 
   onSignInClick = () => {
@@ -56,4 +62,7 @@ class GoogleAuth extends React.Component {
   }
 }
 
-export default GoogleAuth;
+export default connect(
+  null,
+  { signIn, signOut }
+)(GoogleAuth);
