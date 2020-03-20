c4599ca10bcd9c2c039320319b4bace1a631d726
stephen grider <ste.grider@gmail.com>
2020-03-20 13:55:39 -0700

    wire up gapi

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client.init({
+        clientId:
+          '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
+        scope: 'email'
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
