7bf023582f80de331b9610108e4a451dbb3051f5
stephen grider <ste.grider@gmail.com>
2020-03-21 12:16:51 -0700

    pass props

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'streamCreate'
+})(StreamCreate);
