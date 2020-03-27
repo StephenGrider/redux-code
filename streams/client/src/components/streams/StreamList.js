ea39fdc3bbeadc22dac3946b0e294c8f612feb4d
stephen grider <ste.grider@gmail.com>
2020-03-27 14:35:13 -0700

    conditional

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -7,10 +7,22 @@ class StreamList extends React.Component {
     this.props.fetchStreams();
   }
 
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
             {stream.title}
@@ -32,7 +44,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(
