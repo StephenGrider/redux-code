8f804814227b6d2aaa31b5f457f2a84a54fc9227
stephen grider <ste.grider@gmail.com>
2020-03-27 14:09:34 -0700

    fetch

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-const StreamList = () => {
-  return <div>StreamList</div>;
-};
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
 
-export default StreamList;
+  render() {
+    return <div>StreamList</div>;
+  }
+}
+
+export default connect(
+  null,
+  { fetchStreams }
+)(StreamList);
