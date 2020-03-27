48b693f14c9d91f065e98011aca0ca668bef0b71
stephen grider <ste.grider@gmail.com>
2020-03-27 11:58:04 -0700

    handle

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,20 @@
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from '../actions/types';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
