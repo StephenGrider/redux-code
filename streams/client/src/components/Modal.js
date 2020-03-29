8b35a6bb89f99eaa8c0faa7f46402fcc65d21a82
stephen grider <ste.grider@gmail.com>
2020-03-29 11:49:25 -0700

    dismiss

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const Modal = props => {
   return ReactDOM.createPortal(
-    <div
-      onClick={() => history.push('/')}
-      className="ui dimmer modals visible active"
-    >
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
       <div
         onClick={e => e.stopPropagation()}
         className="ui standard modal visible active"
