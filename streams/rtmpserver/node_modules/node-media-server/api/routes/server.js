e48c2f3740d723df4506401e4fea6d1fe20aded4
stephen grider <ste.grider@gmail.com>
2020-03-29 16:31:12 -0700

    oops

--- streams/rtmpserver/node_modules/node-media-server/api/routes/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const serverController = require('../controllers/server');
-
-module.exports = (context) => {
-  let router = express.Router();
-  router.get('/', serverController.getInfo.bind(context));
-  return router;
-};
