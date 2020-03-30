bdfb44e529064bbe86bc83ece1e24ac3e5047f59
stephen grider <ste.grider@gmail.com>
2020-03-30 14:22:37 -0700

    docs

---
+++ docs/precache-manifest.04485e6ac0796ce08d9d1acab11c4532.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "30c55a865f8295a12369",
+    "url": "/static/css/main.2a6c43da.chunk.css"
+  },
+  {
+    "revision": "30c55a865f8295a12369",
+    "url": "/static/js/main.30c55a86.chunk.js"
+  },
+  {
+    "revision": "de15fdff4c0e5085b6df",
+    "url": "/static/js/1.de15fdff.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a2c98990d5a8c1fefe8264a7daa7b07",
+    "url": "/index.html"
+  }
+];
